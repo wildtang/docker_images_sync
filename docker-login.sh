77e525310d2e00a49816acad5a01e4f2ae6db721
adminian-at-jia-nas <adminian@jia-nas.net>
2026-02-23 19:15:37 +0800

    update as moved to adminian

--- docker-login.sh
+++ docker-login.sh
@@ -1,2 +1,3 @@
+export DOCKER_CONFIG=~/.docker/
 qDOCKER=/share/CACHEDEV2_DATA/.qpkg/container-station/bin/docker
 sudo $qDOCKER login --username=yetangye crpi-0a5jcaemfw1596t9.cn-beijing.personal.cr.aliyuncs.com
